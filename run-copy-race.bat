51518b911386226b669ad57d082b6b2a5c3b532c
Andre Stubbe <andrestubbe@proton.me>
2026-06-05 13:58:42 +0200

    Auto Update

--- run-copy-race.bat
+++ run-copy-race.bat
@@ -1,6 +1,9 @@
 ﻿@echo off
-echo ðŸš€ Running Hero Demo...
-cd examples\src
-call mvn compile exec:java -Dexec.mainClass=fastbytes.benchmark.JMH_Copy
+chcp 65001 >nul
+cd /d "%~dp0"
+echo [FastBytes] Running Race Benchmark...
+cd examples
+set MAVEN_OPTS=-Xmx4G
+call mvn compile exec:java -Dexec.mainClass=fastbytes.CopyRace
 cd ..\..
 pause
